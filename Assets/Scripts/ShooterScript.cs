052965330bba404e8a9d10ae969cd51f2fd494ee
memon07 <memonshoyeb26@gmail.com>
2020-04-19 17:49:09 +0530

    shooting arrows

---
+++ Assets/Scripts/ShooterScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    [SerializeField] GameObject projectile , gun;
+
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+}
