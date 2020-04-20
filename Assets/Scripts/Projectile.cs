e26b535584406e02572e5f84de4108b3f0b12471
memon07 <memonshoyeb26@gmail.com>
2020-04-20 14:54:12 +0530

    added defender spawn

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
+    [SerializeField] float damage = 50f;
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var health = collision.GetComponent<Health>();
+        var attacker = collision.GetComponent<Attacker>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
     }
 }
