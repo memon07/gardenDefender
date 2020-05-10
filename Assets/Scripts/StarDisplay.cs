7a7c11094f00da67d00521dd7748c76503612297
memon07 <memonshoyeb26@gmail.com>
2020-05-10 17:37:02 +0530

    added stars in efender trophy

--- Assets/Scripts/StarDisplay.cs
+++ Assets/Scripts/StarDisplay.cs
@@ -27,7 +27,7 @@ public class StarDisplay : MonoBehaviour
 
     public void SpendStars(int amount)
     {
-        if (stars >=amount)
+        if (stars >=amount )
         {
             stars -= amount;
             UpdateDisplay();
